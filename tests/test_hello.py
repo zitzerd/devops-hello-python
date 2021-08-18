35ccf439ec3d75080a9ecf70bbf365669be4042e
Diego Zitzer <zitzerd@hotmail.com>
2021-08-18 11:29:31 -0300

    Add a test for the /hello route that will give OK if the statuscode is 200

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -3,3 +3,8 @@ import pytest
 def test_main_page(client):
     response = client.get('/')
     assert response.data == b'Hello, World'
+
+#Create a test that will fail so i can test githubactions
+def test_hostname_page(client):
+    response = client.get('/host')
+    assert response.status_code == 200
